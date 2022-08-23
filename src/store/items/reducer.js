bfdc2feb24109d8faa38555bee981ca00330ff0f
lucasellery <olucasellery@gmail.com>
2022-08-23 17:27:11 -0300

    (feature) Adding functionalities

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,12 +1,21 @@
+import { ITEM_ADDED } from './actions';
+
 let id = 1;
 
 export const initialItems = [
-  { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
+  { uuid: id++, name: 'Amazing Tofu Roast', price: 14, quantity: 1 },
+  { uuid: id++, name: 'Little potato', price: 6, quantity: 12 }
 ];
 
 export const reducer = (state = initialItems, action) => {
+  if (action.type === ITEM_ADDED) {
+    const item = { uuid: id++, quantity: 1, ...action.payload };
+    return [...state, item];
+  }
+
   return state;
 };
 
 export default reducer;
+
+//mapDispatchToProps
