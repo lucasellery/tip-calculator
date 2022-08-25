7183c90d28ecbbe73d06cdd6e99ddd1c4078650c
lucasellery <olucasellery@gmail.com>
2022-08-24 23:19:25 -0300

    (feature) add connect api | add new functionalities

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,12 +1,15 @@
 import { Stack } from '@twilio-paste/stack';
+import { MenuItemContainer } from '../containers/MenuItemContainer';
 import MenuItem from './MenuItem';
 
 export const MenuItems = ({ items }) => {
   return (
     <Stack orientation="vertical" spacing="space60">
       {items.map((item) => (
-        <MenuItem {...item} key={item.uuid} />
+        <MenuItemContainer {...item} key={item.uuid} />
       ))}
     </Stack>
   );
 };
+
+// Updating Item Data
